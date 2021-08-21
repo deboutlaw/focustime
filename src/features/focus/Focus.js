eba94e49cc3ae36ede393edaf2c407c15ca1b9dc
Deb Outlaw <deborahaoutlaw@gmail.com>
2021-08-21 08:49:56 -0400

    Initial Entry

---
+++ src/features/focus/Focus.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { RoundedButton } from '../../components/RoundedButton';
+import { fontSizes, spacing } from '../../utils/sizes';
+import {colors} from '../../utils/colors';
+//import Constants from 'expo-constants';
+
+export const Focus = ({ addSubject }) => {
+  const [subject, setSubject] = useState(null);
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>What would you like to focus on?</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={{ flex: 1, marginRight: 20 }}
+            onSubmitEditing={({ nativeEvent }) => {
+              setSubject(nativeEvent.text);
+            }}
+          />
+          <RoundedButton
+            size={50}
+            title="+"
+            onPress={() => {
+              addSubject(subject);
+            }}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  inputContainer: {
+    paddingTop: spacing.md,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    color: colors.white,
+    fontWeight: 'bold',
+    fontSize: fontSizes.lg,
+  },
+
+  titleContainer: {
+    flex: 0.5,
+    padding: spacing.md,
+    justifyContent: 'center',
+  },
+});
